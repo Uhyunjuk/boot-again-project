728db57765d1254b59d28f320276b1796b2d50aa
Uhyunjuk <toearthgold@gmail.com>
2024-09-27 20:16:03 +0900

    :sparkles: Feat: 댓글 수정, 삭제 기능 추가

--- src/main/java/com/example/bootagain/service/CommentService.java
+++ src/main/java/com/example/bootagain/service/CommentService.java
@@ -42,4 +42,28 @@ public class CommentService {
         // 4. DTO로 변환해 저장
         return CommentDto.createCommentDto(created);
     }
+
+    @Transactional
+    public CommentDto update(Long id, CommentDto dto) {
+        // 1. 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(() -> new IllegalArgumentException("댓글 수정 실패! " + "대상 댓글이 없습니다."));
+        // 2. 댓글 수정
+        target.patch(dto); // 기존 댓글에 수정 정보 추가
+        // 3. DB 갱신
+        Comment updated = commentRepository.save(target);
+        // 4. 댓글 엔티티를 DTO로 변환 및 컨트롤러에 반환
+        return CommentDto.createCommentDto(updated);
+    }
+
+    @Transactional
+    public CommentDto delete(Long id) {
+        // 1. 댓글 조회 및 예외 발생
+        Comment target = commentRepository.findById(id)
+                .orElseThrow(()-> new IllegalArgumentException("댓글 삭제 실패! " + "대상이 없습니다."));
+        // 2. 댓글 삭제
+        commentRepository.delete(target);
+        // 3. 삭제 댓글을 DTO로 변환 및 반환
+        return CommentDto.createCommentDto(target);
+    }
 }
