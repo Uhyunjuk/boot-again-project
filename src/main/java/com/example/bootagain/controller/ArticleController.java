23896f63ab2cc6a1c3b26032059401b0038929ef
Uhyunjuk <toearthgold@gmail.com>
2024-10-02 18:46:52 +0900

    :sparkles: Feat: 웹 페이지에서 댓글 목록 보이게 추가

--- src/main/java/com/example/bootagain/controller/ArticleController.java
+++ src/main/java/com/example/bootagain/controller/ArticleController.java
@@ -1,8 +1,10 @@
 package com.example.bootagain.controller;
 
 import com.example.bootagain.dto.ArticleForm;
+import com.example.bootagain.dto.CommentDto;
 import com.example.bootagain.entity.Article;
 import com.example.bootagain.repository.ArticleRepository;
+import com.example.bootagain.service.CommentService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -17,11 +19,13 @@ import java.util.List;
 import java.util.Optional;
 
 @Controller
-@Slf4j // 로깅 기능
+@Slf4j
 public class ArticleController {
 
     @Autowired // 스프링부트가 미리 생성해놓은 repository 객체주입(DI)
     private ArticleRepository articleRepository;
+    @Autowired
+    private CommentService commentService;
 
     @GetMapping("/articles/new")
     public String newArticleForm() {
@@ -44,15 +48,16 @@ public class ArticleController {
         return "redirect:/articles/" + saved.getId(); // 리다이렉트 작성위치
     }
 
-    // 단일 데이터 조회하기
     @GetMapping("/articles/{id}")
-    public String show(@PathVariable Long id, Model model) { // 매개변수로 id 받아오기
+    public String show(@PathVariable Long id, Model model) {
         log.info("id = " + id);
         // 1. id를 조회해 데이터 가져오기
-        Article articleEntity = articleRepository.findById(id).orElse(null); // 값이 없으면 null을 저장
+        Article articleEntity = articleRepository.findById(id).orElse(null);
+        List<CommentDto> commentDtos = commentService.comments(id);
 
         // 2. 모델에 데이터 등록하기
         model.addAttribute("article", articleEntity);
+        model.addAttribute("commentDtos", commentDtos);
 
         // 3. 뷰 페이지 반환하기
         return "articles/show";
